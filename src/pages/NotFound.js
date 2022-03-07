894b39a86e36b3b7f7acb507cab85d2eedf3e91f
Débora Serra <debora.r.serra@gmail.com>
2022-03-07 15:29:02 -0300

    Fix page notFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
+import '../styles/NotFound.css';
 
 class NotFound extends React.Component {
   render() {
     return (
       <div data-testid="page-not-found">
-        <p>Page not found (404)</p>
+        <Header />
+        <section className="page-not-found">
+          <img className="notFound" src="https://www.pngitem.com/pimgs/m/116-1163025_music-icon-png-free-download-desenho-de-notas.png" alt="Music symbol" />
+          <p className="not-found-p">Page not found (404)</p>
+        </section>
       </div>
     );
   }
